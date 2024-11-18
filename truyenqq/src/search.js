9d4acb87256ae7f9cca4b64179c7f856d2a21138
ThanhPhuoc91 <117423408+ThanhPhuoc91@users.noreply.github.com>
2024-11-18 13:19:25 +0700

    Update search.js

--- truyenqq/src/search.js
+++ truyenqq/src/search.js
@@ -1,11 +1,14 @@
-load('bypass.js');
 load('config.js');
 function execute(key, page) {
     if (!page) page = '1';
-    var url = BASE_URL +"/tim-kiem/trang-" + page + ".html?q=" + key;
-    var doc = bypass(url, fetch(url).html());
-
-    if (doc) {
+    let response = fetch(BASE_URL +"/tim-kiem.html",{
+        queries: {
+            q: key,
+            page: page
+        }
+    });
+    if (response.ok) {
+        let doc = response.html();
         var novelList = [];
         var next = doc.select(".page_redirect").select("a:has(p.active) + a").last().text();
         doc.select("#main_homepage .list_grid li").forEach(e => {
